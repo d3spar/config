67fa1384a9ee456f510f82e302f5b3c00704e8de
d3spar <hopkinses@gmail.com>
2025-08-09 05:23:35 -0500

    re-add nvim

---
+++ nvim/init_wip.lua
@@ -0,0 +1,34 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.mouse = 'a'
+vim.o.showmode = false
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.signcolumn = 'yes'
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.inccommand = 'split'
+vim.o.cursorline = true
+vim.o.scrolloff = 10
+vim.o.confirm = true
+
+-- [[ Basic Keymaps ]]
+vim.keymap.set('i', '<C-[>', '<Esc>')
+vim.keymap.set('n', '\\', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+vim.pack.add {
+  '',
+}
