1bf1ca99471a39ca5722ae0e9ccd11520124b278
d3spar <hopkinses@gmail.com>
2025-09-27 02:27:51 -0500

    updating nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -507,34 +507,21 @@ require('lazy').setup({
     },
   },
   {
-    'folke/tokyonight.nvim',
+    'scottmckendry/cyberdream.nvim',
+    --   'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('cyberdream').setup {
+        --     require('tokyonight').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
       }
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'cyberdream'
+      --     vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
-
-  {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    ---@type Flash.Config
-    opts = {},
-  -- stylua: ignore
-  keys = {
-    { "<C-s>", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
-  },
-
   {
     'stevearc/oil.nvim',
     ---@module 'oil'
@@ -615,7 +602,7 @@ require('lazy').setup({
       indent = { enable = true, disable = { 'ruby' } },
     },
   },
-  require 'plugins.debug',
+  -- require 'plugins.debug',
   require 'plugins.lint',
   require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
 }, {
