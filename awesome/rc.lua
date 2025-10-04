051f6c3ee12fb1198882d57cc7a0ae9ec9d866f5
d3spar <hopkinses@gmail.com>
2025-10-04 10:13:29 -0500

    add keybind to open file explorer window to awesomewm rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -374,6 +374,9 @@ globalkeys = gears.table.join(
 	awful.key({ modkey }, "Return", function()
 		awful.spawn(terminal)
 	end, { description = "open a terminal", group = "launcher" }),
+	awful.key({ modkey }, "e", function()
+		awful.spawn("thunar")
+	end, { description = "open file explorer", group = "launcher" }),
 	awful.key({ modkey, "Control" }, "r", awesome.restart, { description = "reload awesome", group = "awesome" }),
 	awful.key({ modkey, "Shift" }, "q", awesome.quit, { description = "quit awesome", group = "awesome" }),
 
