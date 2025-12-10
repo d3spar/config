8f146f09f7685341753f2f9426fc4880faba82f1
d3spar <hopkinses@gmail.com>
2025-12-10 00:26:46 -0600

    fix typo

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,7 @@ vim.o.signcolumn = 'yes'
 vim.o.updatetime = 250
 vim.o.timeoutlen = 400
 vim.o.splitbelow = true
-vim.ot.splitright = true
+vim.o.splitright = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.o.cursorline = true
@@ -335,14 +335,10 @@ require('lazy').setup({
         rust_analyzer = {},
         zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
-        --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
-        --
-
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
