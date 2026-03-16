cdd4436a9d57baddfba776aec8f3eee1de958581
d3spar <hopkinses@gmail.com>
2026-03-16 01:09:32 -0500

    formatting and update of installed lsp's

--- nvim/init.lua
+++ nvim/init.lua
@@ -543,7 +543,27 @@ require('lazy').setup({
     branch = 'main',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
-      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'javascript' }
+      local parsers = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'python',
+        'javascript',
+        'typescript',
+        'go',
+        'rust',
+        'zig',
+        'cpp',
+        'cuda',
+      }
       require('nvim-treesitter').install(parsers)
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
